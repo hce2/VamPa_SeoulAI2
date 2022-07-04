98a142f657a12506304df15a64ade1f034b9ad21
hce2 <codms228@gmail.com>
2022-07-04 11:38:37 +0900

    환경설정3 톰캣연결, 한글깨짐 해결

--- VamPa_Board(Oracle)/src/test/java/com/vam/persistence/DataSourceTest.java
+++ VamPa_Board(Oracle)/src/test/java/com/vam/persistence/DataSourceTest.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +19,26 @@ public class DataSourceTest {
 	@Autowired
     private DataSource dataSource;
     
+    @Autowired 
+    private SqlSessionFactory sqlSessionFactory;
+    
     @Test
     public void testConnection() {
-      
-        try(Connection con = dataSource.getConnection();){
+        
+        try(
+                Connection con = dataSource.getConnection();
+                SqlSession session = sqlSessionFactory.openSession();
             
-            System.out.println("con = " + con);
+            ){
             
-        } catch(Exception e) {
+                System.out.println("con = " + con);
+                System.out.println("session = " + session);
             
-            e.printStackTrace();
+            } catch(Exception e) {
             
-        }
+                e.printStackTrace();
+            
+            }
         
     }
 }
